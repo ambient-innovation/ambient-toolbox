aac148e6d37f418581efa15c71bfffca8b019edd
Alexander Grill <alexander.grill@ambient.digital>
2023-05-05 13:22:42 +0200

    lint

--- tests/sentry/mock_data.py
+++ tests/sentry/mock_data.py
@@ -20,9 +20,10 @@ SENTRY_EVENT = {
                                 'self': '<testapp.account.api.views.TestViewSet object>',
                                 'serializer': 'TestDetailSerializer(<User: Test AAAA>)',
                                 '__class__': "<class 'test.account.api.views.TestViewSet'>",
-                                'admin': '<EmailUser: Test User>', 'test_data': '123'
+                                'admin': '<EmailUser: Test User>',
+                                'test_data': '123',
                             },
-                            'in_app': True
+                            'in_app': True,
                         }
                     ]
                 },
@@ -30,27 +31,31 @@ SENTRY_EVENT = {
         ]
     },
     'request': {
-        'url': 'http://localhost:8000/api/v2/test/17/', 'query_string': '', 'method': 'PUT',
+        'url': 'http://localhost:8000/api/v2/test/17/',
+        'query_string': '',
+        'method': 'PUT',
         'env': {'SERVER_NAME': 'cdc3cb5b00af', 'SERVER_PORT': '8000', 'REMOTE_ADDR': '172.22.0.1'},
         'headers': {
-            'Content-Length': '1202', 'Content-Type': 'application/json', 'Host': 'localhost:8000',
+            'Content-Length': '1202',
+            'Content-Type': 'application/json',
+            'Host': 'localhost:8000',
             'User-Agent': 'Mozilla/5.0 (Macintosh;) Gecko/20100101 Firefox/112.0',
-            'Accept': 'application/json, text/plain, */*', 'Accept-Language': 'en',
+            'Accept': 'application/json, text/plain, */*',
+            'Accept-Language': 'en',
             'Accept-Encoding': 'gzip, deflate, br',
             'Authorization': 'Token 123123123123123123123123123123123123abcd',
-            'Origin': 'http://127.0.0.1:3000', 'Dnt': '1', 'Connection': 'keep-alive',
-            'Referer': 'http://127.0.0.1:3000/', 'Sec-Fetch-Dest': 'empty', 'Sec-Fetch-Mode': 'cors',
-            'Sec-Fetch-Site': 'cross-site'
+            'Origin': 'http://127.0.0.1:3000',
+            'Dnt': '1',
+            'Connection': 'keep-alive',
+            'Referer': 'http://127.0.0.1:3000/',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'cross-site',
         },
         'cookies': {},
-        'data': ''
+        'data': '',
     },
-    'user': {
-        'email': 'test@test.local',
-        'id': '1337',
-        'ip_address': '172.22.0.1',
-        'username': 'test@test.local'
-    }
+    'user': {'email': 'test@test.local', 'id': '1337', 'ip_address': '172.22.0.1', 'username': 'test@test.local'},
 }
 
 SCRUBBED_SENTRY_EVENT = {
@@ -61,10 +66,7 @@ SCRUBBED_SENTRY_EVENT = {
                 'module': None,
                 'type': 'ZeroDivisionError',
                 'value': 'division by zero',
-                'mechanism': {
-                    'type': 'django',
-                    'handled': False
-                },
+                'mechanism': {'type': 'django', 'handled': False},
                 'stacktrace': {
                     'frames': [
                         {
@@ -79,9 +81,9 @@ SCRUBBED_SENTRY_EVENT = {
                                 'serializer': '[Filtered]',  # filtered due to standard filter
                                 '__class__': '"<class \'test.account.api.views.TestViewSet\'>"',
                                 'admin': '[Filtered]',  # filtered due to standard filter
-                                'test_data': "'123'"
+                                'test_data': "'123'",
                             },
-                            'in_app': True
+                            'in_app': True,
                         }
                     ]
                 },
@@ -89,7 +91,9 @@ SCRUBBED_SENTRY_EVENT = {
         ]
     },
     'request': {
-        'url': 'http://localhost:8000/api/v2/test/17/', 'query_string': '', 'method': 'PUT',
+        'url': 'http://localhost:8000/api/v2/test/17/',
+        'query_string': '',
+        'method': 'PUT',
         'env': {'SERVER_NAME': 'cdc3cb5b00af', 'SERVER_PORT': '8000', 'REMOTE_ADDR': '172.22.0.1'},
         'headers': {
             'Content-Length': '1202',
@@ -106,16 +110,16 @@ SCRUBBED_SENTRY_EVENT = {
             'Referer': 'http://127.0.0.1:3000/',
             'Sec-Fetch-Dest': 'empty',
             'Sec-Fetch-Mode': 'cors',
-            'Sec-Fetch-Site': 'cross-site'
+            'Sec-Fetch-Site': 'cross-site',
         },
         'cookies': {},
-        'data': ''
+        'data': '',
     },
     'user': {
         'email': '[Filtered]',  # filtered due to default sentry filter
         'id': '1337',
         'ip_address': '[Filtered]',  # filtered due to default sentry filter
-        'username': '[Filtered]'  # filtered due to default sentry filter
+        'username': '[Filtered]',  # filtered due to default sentry filter
     },
     '_meta': {
         'exception': {
@@ -125,22 +129,8 @@ SCRUBBED_SENTRY_EVENT = {
                         'frames': {
                             '0': {
                                 'vars': {
-                                    'serializer': {
-                                        '': {
-                                            'rem': [[
-                                                '!config',
-                                                's'
-                                            ]]
-                                        }
-                                    },
-                                    'admin': {
-                                        '': {
-                                            'rem': [[
-                                                '!config',
-                                                's'
-                                            ]]
-                                        }
-                                    }
+                                    'serializer': {'': {'rem': [['!config', 's']]}},
+                                    'admin': {'': {'rem': [['!config', 's']]}},
                                 }
                             }
                         }
@@ -148,46 +138,11 @@ SCRUBBED_SENTRY_EVENT = {
                 }
             }
         },
-        'request': {
-            'headers': {
-                'Authorization': {
-                    '': {
-                        'rem': [[
-                            '!config',
-                            's'
-                        ]]
-                    }
-                }
-            }
-        },
+        'request': {'headers': {'Authorization': {'': {'rem': [['!config', 's']]}}}},
         'user': {
-            'email': {
-                '': {
-                    'rem':
-                        [[
-                            '!config',
-                            's'
-                        ]]
-                }
-            },
-            'ip_address': {
-                '': {
-                    'rem':
-                        [[
-                            '!config',
-                            's'
-                        ]]
-                }
-            },
-            'username': {
-                '': {
-                    'rem':
-                        [[
-                            '!config',
-                            's'
-                        ]]
-                }
-            }
-        }
-    }
+            'email': {'': {'rem': [['!config', 's']]}},
+            'ip_address': {'': {'rem': [['!config', 's']]}},
+            'username': {'': {'rem': [['!config', 's']]}},
+        },
+    },
 }
