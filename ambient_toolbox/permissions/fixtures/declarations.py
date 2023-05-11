fb2bd2699b8bbe82f2735c7d89f31d7d49215e30
Ronny Vedrilla <ronny.vedrilla@ambient-innovation.com>
2023-05-11 19:30:58 +0200

    v8.3.2

--- ambient_toolbox/permissions/fixtures/declarations.py
+++ ambient_toolbox/permissions/fixtures/declarations.py
@@ -1,14 +1,15 @@
 import dataclasses
+from typing import List
 
 
 @dataclasses.dataclass
 class PermissionModelDeclaration:
     app_label: str
-    codename_list: list[str]
+    codename_list: List[str]
     model: str
 
 
 @dataclasses.dataclass
 class GroupPermissionDeclaration:
     name: str
-    permission_list: list[PermissionModelDeclaration]
+    permission_list: List[PermissionModelDeclaration]
