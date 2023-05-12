581298a19ec98c25ff4f389275355e143efbf0ce
Ronny Vedrilla <ronny.vedrilla@ambient-innovation.com>
2023-05-12 08:09:55 +0200

    Fixed Python 3.8 compat

--- ambient_toolbox/permissions/fixtures/helpers.py
+++ ambient_toolbox/permissions/fixtures/helpers.py
@@ -1,4 +1,7 @@
-def generate_default_permissions(model_name: str) -> list[str]:
+from typing import List
+
+
+def generate_default_permissions(model_name: str) -> List[str]:
     return [
         f'add_{model_name}',
         f'change_{model_name}',
--- ambient_toolbox/permissions/fixtures/services.py
+++ ambient_toolbox/permissions/fixtures/services.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
 from django.db import transaction
@@ -16,7 +18,7 @@ class PermissionSetupService:
         self.dry_run = dry_run
 
     @transaction.atomic
-    def process(self):
+    def process(self) -> (List[Permission], List[Permission]):
         # Fetch or create group
         group, created = Group.objects.get_or_create(name=self.group_declaration.name)
 
