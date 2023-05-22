539202dbbba41915bc2fdf9481de3e99668355f7
Michael Agel <michael.agel@ambient.digital>
2023-05-22 10:54:04 +0000

    emit DeprecationWarning when using CurrentUserMiddleware

--- ambient_toolbox/middleware/current_user.py
+++ ambient_toolbox/middleware/current_user.py
@@ -1,5 +1,28 @@
-from .current_request import CurrentRequestMiddleware as CurrentUserMiddleware
+import warnings
+from typing import TYPE_CHECKING, Callable
 
-__all__ = [
-    'CurrentUserMiddleware',
-]
+from .current_request import CurrentRequestMiddleware
+
+if TYPE_CHECKING:
+    from django.http import HttpRequest, HttpResponse
+
+
+class CurrentUserMiddleware(CurrentRequestMiddleware):
+    """
+    Middleware which stores the current user in a thread-safe manner.
+
+    This middleware is deprecated, use CurrentRequestMiddleware instead.
+    """
+
+    # Tech Note: This class is now a mere alias for CurrentRequestMiddleware to
+    # provide backward compatibility. This whole module can be removed as part
+    # of one of the next major releases, then fully dropping support for
+    # CurrentUserMiddleware.
+
+    def __init__(self, get_response: Callable[['HttpRequest'], 'HttpResponse']):
+        warnings.warn(
+            "CurrentUserMiddleware is deprecated. Use CurrentRequestMiddleware instead.",
+            category=DeprecationWarning,
+            stacklevel=1,
+        )
+        super().__init__(get_response)
