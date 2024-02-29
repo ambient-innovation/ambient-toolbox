908db36f5db189ed1709967b49d3f50574ecc707
Ronny Vedrilla <ronny.vedrilla@ambient.digital>
2024-02-29 14:26:49 +0100

    v9.6.0

--- tests/management_commands/test_create_translation_file.py
+++ tests/management_commands/test_create_translation_file.py
@@ -1,4 +1,5 @@
 from os.path import isfile
+from pathlib import Path
 
 from django.conf import settings
 from django.core.management import call_command
@@ -6,6 +7,11 @@ from django.test import SimpleTestCase
 
 
 class CreateTranslationFileTest(SimpleTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        Path(settings.LOCALE_PATHS[0] / "xx/LC_MESSAGES").mkdir(parents=True, exist_ok=True)
+
     def test_command_regular(self):
         call_command("create_translation_file", lang="xx")
 
