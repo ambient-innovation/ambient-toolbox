db2da98130f94d6aa413d862d02a4159efb58c8c
Robin Zachmann <robin.zachmann@ambient.digital>
2024-07-15 14:42:04 +0200

    feat: add function `get_cached_related_obj()`

--- ambient_toolbox/utils/model.py
+++ ambient_toolbox/utils/model.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from django.db.models import ForeignKey
+from django.db.models import ForeignKey, Model
 
 
 def object_to_dict(obj, blacklisted_fields: Optional[list] = None, include_id: bool = False) -> dict:
@@ -30,3 +30,42 @@ def object_to_dict(obj, blacklisted_fields: Optional[list] = None, include_id: b
             valid_data[key] = value
 
     return valid_data
+
+
+def get_cached_related_obj(obj: Model, related_field_name: str, silently_return_none: bool = False):
+    """
+    This function helps to avoid silent sub-queries, due to missing `select_related()` or `prefetch_related()`.
+
+    This function performs a lookup in the fields_cache of the given object.
+    If silently_return_none is True, this function will return None if the field is not cached.
+    Otherwise, an AttributeError will be raised.
+
+    What problem does this function solve?
+
+    foo = Foo.objects.all().get()
+    foo.bar  # <-- this will silently cause a sub-query
+
+    foo = Foo.objects.all().select_related('bar').first()
+    foo.bar # <-- this will cause no sub-query
+
+    The only difference is the usage of `select_related()`, which can easily be forgotten.
+
+    Usage:
+    get_cached_related_obj(foo, 'bar')
+
+    This will result in a KeyError instead of a silent sub-query if the field is not cached
+    via `select_related()` or `prefetch_related()`.
+    """
+    # warning: this is an undocumented feature of django
+    fields_cache = obj._state.fields_cache
+
+    if related_field_name in fields_cache:
+        return fields_cache[related_field_name]
+
+    if silently_return_none:
+        return None
+
+    raise AttributeError(
+        f'Field "{related_field_name}" not found in `fields_cache` of {obj.__class__.__name__} object. '
+        "Did you forget to use `select_related()` or `prefetch_related()`?"
+    )
--- tests/test_utils_model.py
+++ tests/test_utils_model.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.models import Permission
 from django.test import TestCase
 
-from ambient_toolbox.utils import object_to_dict
+from ambient_toolbox.utils import get_cached_related_obj, object_to_dict
 from testapp.models import ForeignKeyRelatedModel, MySingleSignalModel
 
 
@@ -29,3 +30,57 @@ class UtilModelTest(TestCase):
 
         self.assertIn("single_signal_id", valid_data)
         self.assertEqual(valid_data["single_signal_id"], obj.id)
+
+    # --- Test get_cached_related_obj (happy path) ---
+    def test_get_cached_related_obj_with_select_related(self):
+        permission_obj = Permission.objects.select_related("content_type").first()
+        with self.assertNumQueries(0):
+            content_type = get_cached_related_obj(permission_obj, "content_type")
+            self.assertIsNotNone(content_type)
+
+    def test_get_cached_related_obj_with_prefetch_related(self):
+        permission_obj = Permission.objects.prefetch_related("content_type").first()
+        with self.assertNumQueries(0):
+            content_type = get_cached_related_obj(permission_obj, "content_type")
+            self.assertIsNotNone(content_type)
+
+    def test_get_cached_related_obj_without_cached_and_silently_return_none(self):
+        permission_obj = Permission.objects.first()
+        with self.assertNumQueries(0):
+            content_type = get_cached_related_obj(permission_obj, "content_type", silently_return_none=True)
+            self.assertIsNone(content_type)
+
+    # --- Test get_cached_related_obj (error path) ---
+
+    def test_get_cached_related_obj_without_cached(self):
+        permission_obj = Permission.objects.first()
+        with self.assertNumQueries(0):
+            with self.assertRaises(AttributeError) as ctx:
+                get_cached_related_obj(permission_obj, "content_type")
+            self.assertEqual(
+                f'Field "content_type" not found in `fields_cache` of {permission_obj.__class__.__name__} object. '
+                "Did you forget to use `select_related()` or `prefetch_related()`?",
+                str(ctx.exception),
+            )
+
+    def test_get_cached_related_obj_with_non_existing_field(self):
+        permission_obj = Permission.objects.first()
+        with self.assertNumQueries(0):
+            with self.assertRaises(AttributeError) as ctx:
+                get_cached_related_obj(permission_obj, "foo")
+            self.assertEqual(
+                f'Field "foo" not found in `fields_cache` of {permission_obj.__class__.__name__} object. '
+                "Did you forget to use `select_related()` or `prefetch_related()`?",
+                str(ctx.exception),
+            )
+
+    def test_get_cached_related_obj_with_no_fk_field(self):
+        permission_obj = Permission.objects.first()
+        with self.assertNumQueries(0):
+            with self.assertRaises(AttributeError) as ctx:
+                get_cached_related_obj(permission_obj, "name")
+            self.assertEqual(
+                f'Field "name" not found in `fields_cache` of {permission_obj.__class__.__name__} object. '
+                "Did you forget to use `select_related()` or `prefetch_related()`?",
+                str(ctx.exception),
+            )
