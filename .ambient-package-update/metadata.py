1e0aec96f1614c6d61b2dd8692312b1967b32527
Ronny Vedrilla <ronny.vedrilla@ambient.digital>
2025-02-03 12:09:34 +0100

    Coverage

--- .ambient-package-update/metadata.py
+++ .ambient-package-update/metadata.py
@@ -21,7 +21,7 @@ METADATA = PackageMetadata(
         ),
     ],
     maintainer=PackageMaintainer(name="Ambient Digital", url="https://ambient.digital/", email="hello@ambient.digital"),
-    min_coverage=72.02,
+    min_coverage=71.75,
     development_status="5 - Production/Stable",
     license=LICENSE_MIT,
     license_year=2012,
