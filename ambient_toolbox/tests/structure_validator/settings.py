4af9d461f6e910f7c869a76449fa55a72773dc14
christoph-teichmeister <christoph.teichmeister@gmail.com>
2025-06-11 10:23:41 +0200

    refactor: deprecate whitelist in settings
    Introduced `allowlist` to replace `whitelist` for consistency and inclusivity. Added deprecation warning for the `whitelist` usage, maintaining backward compatibility.
    
    - Added `TEST_STRUCTURE_VALIDATOR_FILE_ALLOWLIST` as the new setting.
    - Deprecated `TEST_STRUCTURE_VALIDATOR_FILE_WHITELIST` with a warning.
    - Ensured support for existing `whitelist` usage temporarily.

--- ambient_toolbox/tests/structure_validator/settings.py
+++ ambient_toolbox/tests/structure_validator/settings.py
@@ -2,6 +2,47 @@ from django.conf import settings
 
 # Test validator
 TEST_STRUCTURE_VALIDATOR_FILE_ALLOWLIST = []
+_TEST_STRUCTURE_VALIDATOR_FILE_WHITELIST = []
+
+
+@property
+def TEST_STRUCTURE_VALIDATOR_FILE_WHITELIST():  # noqa: N802
+    """
+    The term "Whitelist" will be deprecated in 12.2 and move to "Allowlist".
+    Until then, keep this for backwards compatibility but warn users about future deprecation.
+    """
+
+    import warnings
+
+    warnings.warn(
+        "The 'TEST_STRUCTURE_VALIDATOR_FILE_WHITELIST' setting is deprecated and will be removed in a future version."
+        "Use 'TEST_STRUCTURE_VALIDATOR_FILE_ALLOWLIST' instead.",
+        category=DeprecationWarning,
+        stacklevel=1,
+    )
+    return _TEST_STRUCTURE_VALIDATOR_FILE_WHITELIST
+
+
+@TEST_STRUCTURE_VALIDATOR_FILE_WHITELIST.setter
+def TEST_STRUCTURE_VALIDATOR_FILE_WHITELIST(value):  # noqa: N802
+    """
+    The term "Whitelist" will be deprecated in 12.2 and move to "Allowlist".
+    Until then, keep this for backwards compatibility but warn users about future deprecation.
+    """
+
+    import warnings
+
+    from django.conf import settings
+
+    warnings.warn(
+        "The 'TEST_STRUCTURE_VALIDATOR_FILE_WHITELIST' setting is deprecated and will be removed in a future version."
+        "Use 'TEST_STRUCTURE_VALIDATOR_FILE_ALLOWLIST' instead.",
+        category=DeprecationWarning,
+        stacklevel=1,
+    )
+    settings._TEST_STRUCTURE_VALIDATOR_FILE_WHITELIST = value
+
+
 try:
     TEST_STRUCTURE_VALIDATOR_BASE_DIR = settings.BASE_DIR
 except AttributeError:
